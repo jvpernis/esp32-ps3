da903427efa26b3052da3515cd467a495624dca8
Jeffrey van Pernis <git@jeffreyvanpernis.nl>
2019-07-12 22:13:22 +0200

    Fixed ESP-IDF include paths after they refactored the bluetooth component

--- component.mk
+++ component.mk
@@ -2,6 +2,15 @@
 # Component Makefile
 #
 
+COMPONENT_EXTRA_INCLUDES +=     $(IDF_PATH)/components/bt/common/include/                     \
+                                $(IDF_PATH)/components/bt/host/bluedroid/common/include/      \
+                                $(IDF_PATH)/components/bt/host/bluedroid/stack/include        \
+                                $(IDF_PATH)/components/bt/host/bluedroid/stack/gap/include    \
+                                $(IDF_PATH)/components/bt/host/bluedroid/stack/l2cap/include  \
+                                $(IDF_PATH)/components/bt/host/bluedroid/osi/include
+
+# Legacy bluedroid location include paths
+# i.e. ESP-IDF prior to revision 21165edf41813482bd1d0a8183969854d1011f24
 COMPONENT_EXTRA_INCLUDES +=     $(IDF_PATH)/components/bt/bluedroid/common/include/           \
                                 $(IDF_PATH)/components/bt/bluedroid/stack/include             \
                                 $(IDF_PATH)/components/bt/bluedroid/stack/gap/include         \
